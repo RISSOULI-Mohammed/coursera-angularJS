f9dfa7450f59da06d38e4c2fab5d30d20ea6e73d
RISSOULI-Mohammed <simo.rissouli@hotmail.com>
2018-10-11 20:28:07 +0100

    Assignment 1

--- mod1_solution-Opt/app.js
+++ mod1_solution-Opt/app.js
@@ -6,6 +6,8 @@
     function lunchCheckController($scope) {
         $scope.items = "";
         $scope.message = "";
+        $scope.color = "";
+        $scope.bcolor = "";
         $scope.check = function(){
             var arr = $scope.items.split(",");
             var count = 0;
@@ -16,10 +18,16 @@
             }
             if (count == 0) {
                 $scope.message = "Please enter data first";
+                $scope.color = "red";
+                $scope.bcolor = "red";
             } else if (count < 4) {
                 $scope.message = "Enjoy!";
+                $scope.color = "green";
+                $scope.bcolor = "green";
             } else {
                 $scope.message = "Too much!";
+                $scope.color = "green";
+                $scope.bcolor = "green";
             }
         }
     }
